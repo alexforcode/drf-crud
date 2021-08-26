bef45dcd9b5f22862d0ba5eeecb3cb6470eabd7c
Alex Code <alex.for.code@gmail.com>
2021-08-26 16:28:47 +0300

    Add CRUD API using ModelViewSets

--- books/urls.py
+++ books/urls.py
@@ -1,8 +1,14 @@
-from django.urls import path
+from rest_framework.routers import DefaultRouter
+
+from .views import AuthorViewSet, BookViewSet
 
 
 app_name = 'books'
 
-urlpatterns = [
+router = DefaultRouter()
+router.register('authors', AuthorViewSet, basename='author')
+router.register('books', BookViewSet, basename='book')
 
+urlpatterns = [
+    *router.urls,
 ]
--- books/views.py
+++ books/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from .models import Author, Book
+from .permissions import IsAdminUserOrReadOnly
+from .serializers import AuthorSerializer, BookSerializer
+
+
+class BookViewSet(ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = (IsAdminUserOrReadOnly, )
+
+
+class AuthorViewSet(ModelViewSet):
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
+    permission_classes = (IsAdminUserOrReadOnly, )
